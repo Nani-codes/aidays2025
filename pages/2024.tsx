a6b16ac544cd3a9ff905a18ea1115647c88dc07c
Nani <lakshminarayanamalineni1@gmail.com>
2024-08-03 20:06:51 +0530

    2024 URL Masking

---
+++ pages/2024.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const _2024: React.FC = () => {
+  return (
+    <div style={{ width: '100%', height: '100vh', overflow: 'hidden' }}>
+      <iframe 
+        src="https://aidays2024.vercel.app" 
+        title="External Site" 
+        width="100%" 
+        height="100%"
+        style={{ border: 'none' }}
+      />
+    </div>
+  );
+};
+
+export default _2024;
